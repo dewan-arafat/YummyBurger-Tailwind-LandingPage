5384febe40d90c46459616795be39f9abb32cf53
dewan-arafat <dewanarafat1@gmail.com>
2023-11-03 21:49:46 +0600

    promo menu review added

--- src/main.js
+++ src/main.js
@@ -15,3 +15,45 @@ hamburger.addEventListener("click", ()=>{
 closeBtn.addEventListener("click", ()=>{
     navMenu.classList.add('hidden');
 })
+
+// menu tabs
+const tabs=document.querySelectorAll(".menu_tabs ul li");
+const all= document.querySelectorAll(".items_wrap");
+const foods= document.querySelectorAll(".food");
+const snacks= document.querySelectorAll(".snacks");
+const bevarages= document.querySelectorAll(".bevarage");
+
+tabs.forEach(tab=>{
+    tab.addEventListener('click',()=>{
+        tabs.forEach(tab=>{
+            tab.classList.remove("active");
+        })
+        tab.classList.add("active");
+        const tabValue= tab.getAttribute("data_tabs");
+        console.log(tabValue);
+
+        all.forEach(item=>{
+            item.style.display='none';
+        })
+        if (tabValue==='food') {
+             foods.forEach(food=>{
+            food.style.display='block';
+        })
+        }
+        else if(tabValue==='snack') {
+            snacks.forEach(snack=>{
+           snack.style.display='block';
+       })
+       }
+        else if(tabValue==='bevarage') {
+        bevarages.forEach(bevarage=>{
+        bevarage.style.display='block';
+        })
+        }
+        else{
+            all.forEach(item=>{
+           item.style.display='block';
+            })
+            }
+    })
+})
